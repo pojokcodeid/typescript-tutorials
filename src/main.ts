51e4ce48bfab92fead2a5d8a87419683b5386997
asep komarudin <pojokcodeid@gmail.com>
2023-06-26 23:07:16 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+class Employee {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  public greet(): void {
+    console.log("Hello, I am ".concat(this.name));
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+}
+
+let emp = new Employee("Rizky");
+emp.greet();
+console.log(emp.getName());
+emp.setName("Rizky Kurniawan");
+console.log(emp.getName());
